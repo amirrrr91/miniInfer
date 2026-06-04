b399f277006a27052365c801872c7a2fce54ca1d
amirrrr91 <amiirrrrayman0@email.com>
2026-06-04 22:25:54 +0000

    Phase 1 complete

---
+++ scripts/export_sample.py
@@ -0,0 +1,24 @@
+import torch
+import struct
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+test_data = datasets.MNIST('./data', train=False, download=False, transform=transform)
+
+for i in range(10):
+    image, label = test_data[i * 100]  # spread samples across dataset
+
+    pixels = image.view(-1).numpy()
+
+    filename = f'weights/sample_{i}.bin'
+
+    with open(filename, 'wb') as f:
+        f.write(struct.pack('I', label))
+        f.write(pixels.tobytes())
+
+    print(f"Exported: {filename} — true label: {label}")
+    print(f"First pixel value: {pixels[0]:.6f}")
--- src/main.c
+++ src/main.c
@@ -8,19 +8,25 @@ vboxuser@ubuntu-24:~/miniInfer$  ls -la weights/model.bin
 -rw-rw-r-- 1 vboxuser vboxuser 407116 Mar 30 20:47 weights/model.bin
 407116 / 4 = 101779 floats
 */
-#define ARENA_SIZE (110000 * sizeof(float))
+#define ARENA_SIZE (10 * 1024 * 1024)  // 10 MB
 
 int main(void) {
     Arena a;
     int out ;
+    char path[64];
     ArenaInit(&a ,ARENA_SIZE ) ;
     Network *network = load_network("weights/model.bin", &a);
-    Tensor  *inputs  = load_sample ("weights/sample.bin",&out,&a);
+    for (int i =0 ; i< 10 ;i++)
+    {
+
+    sprintf(path, "weights/sample_%d.bin", i);
+    Tensor  *inputs  = load_sample (path,&out,&a);
 
     int prediction = forward_pass(network, inputs, &a);
     printf("True label:  %d\n", out);
     printf("Predicted:   %d\n", prediction);
     printf("%s\n", prediction == out ? "CORRECT" : "WRONG");
-    
+    }
+    ArenaReset(&a);   
     return 0;
 }
