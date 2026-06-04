c4e7d62c6c5e7d8a88d41934b55343e3b66a8a6d
amirrrr91 <amiirrrrayman0@gmail.com>
2026-06-04 19:21:13 +0300

    loading samples

--- src/main.c
+++ src/main.c
@@ -6,9 +6,39 @@ vboxuser@ubuntu-24:~/miniInfer$  ls -la weights/model.bin
 407116 / 4 = 101779 floats
 */
 #define ARENA_SIZE (110000 * sizeof(float))
+ Tensor *load_sample(const char *path ,int *out, Arena *a);
 int main(void) {
     Arena a;
+    int out ;
     ArenaInit(&a ,ARENA_SIZE ) ;
     Network *network = load_network("weights/model.bin", &a);
+    Tensor  *inputs  = load_sample ("weights/sample.bin",&out,&a);
+
+    int prediction = forward_pass(network, inputs, &a);
+    printf("True label:  %d\n", out);
+    printf("Predicted:   %d\n", prediction);
+    printf("%s\n", prediction == out ? "CORRECT" : "WRONG");
+    
     return 0;
 }
+
+ Tensor *load_sample(const char *path ,int *out, Arena *a){
+
+   FILE *file = fopen(path ,"rb");
+   if (file == NULL){
+      fprintf(stderr,"Error opening'%s'\n",path);
+      return NULL ;
+      }
+
+  /* ── verify header ─────────────────────────────────────────── */
+   uint32_t label ;  
+   Tensor *pixels = make_tensor(a , 1 , 784);
+
+   fread(&label, sizeof(uint32_t), 1, file);
+   fread(pixels->data ,784* sizeof(float), 1 ,file);
+
+   *out = (int)label;   // add this line
+   fclose(file);
+
+   return pixels ;
+ }
